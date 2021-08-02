86fc7cf2c3197c0dde422854dfda0a806fb9569c
NP <NP@NOVINPENDAR>
2021-08-02 21:59:58 +0430

    final

--- matrix_vector_multiplication.cpp
+++ matrix_vector_multiplication.cpp
@@ -1,7 +1,65 @@
 #include <iostream>
+#include <omp.h>
+#include <stdio.h>
+
+#define M 5
+#define N 8
 
 int main()
 {
-	
+	int y[M],x[N],z[M];
+	int A[M * N];
+	// y = Ax
+	int i, j;
+	for ( i = 0; i < N; i++)
+	{
+		x[i] = rand();
+	}
+	printf("X :\n");
+	for (j = 0; j < M; j++)
+	{
+		printf("%d\t", x[j]);
+	}
+	printf("\n");
+	printf("A :\n");
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j < M; j++)
+		{
+			A[j + i * M] = rand();
+		}
+	}
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j < M; j++)
+		{
+			printf("%d\t", A[j + i * M]);
+		}
+		printf("\n");
+	}
+#pragma omp parallel private(j,z)
+	{
+		for (j = 0; j < M; j++)
+			z[j] = 0;
+		#pragma omp for
+		for (i = 0; i < N; i++)
+		{
+			for (j = 0; j < M; j++)
+			{
+				z[j] += A[j + i * M] * x[i];
+			}
+		}
+		#pragma omp critical
+		for (j = 0; j < M; j++)
+		{
+			y[j] += z[j];
+		}
+	}
+	printf("\n");
+	printf("Y :\n");
+	for (j = 0; j < M; j++)
+	{
+		printf("%d\t",y[j]);
+	}
+	return 0;
 }
-
